cb38571a0adcd384fe5932c3dd4b4495bdf71526
Azerofth <mylessoh@gmail.com>
2024-03-13 20:25:04 +0800

    all account stuff

--- frontcomposer/src/app/app-routing.module.ts
+++ frontcomposer/src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AboutComponent } from './about/about.component';
+import { PcbuilderComponent } from './pcbuilder/pcbuilder.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 const routes: Routes =[
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent},
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent},
+  { path: 'pcbuilder', component: PcbuilderComponent},
+  { path: 'registration', component: RegistrationComponent}
 ]
 
 @NgModule({
