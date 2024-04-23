0b0510cef74e8930186ccafed506e8cba254f0dc
Azerofth <mylessoh@gmail.com>
2024-04-23 22:48:06 +0800

    7 days to commit

--- frontcomposer/src/app/dashboard/dashboard.component.ts
+++ frontcomposer/src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-  items: MenuItem[] | undefined;
-  activeItem: MenuItem | undefined;
-  ngOnInit() {
-    this.items = [
-        { label: 'Home', icon: 'pi pi-fw pi-home' },
-        { label: 'Build a computer', icon: 'pi pi-fw pi-calendar' },
-        { label: 'Edit a computer', icon: 'pi pi-fw pi-pencil' },
-        { label: 'Settings', icon: 'pi pi-fw pi-cog' }
-    ];
 
-    this.activeItem = this.items[0];
-}
+export class DashboardComponent {
+  
+  menuItems: MenuItem[] = [
+    { label: 'Home', icon: 'pi pi-fw pi-home', command: () => this.selectItem(1) },
+    { label: 'Edit', icon: 'pi pi-fw pi-file-edit',command: () => this.selectItem(2) },
+    { label: 'Settings', icon: 'pi pi-fw pi-cog', command: () => this.selectItem(3) }
+  ];
+
+  selectedItem: number = 1;
+
+  selectItem(item: number) {
+    this.selectedItem = item;
+  }
 }
